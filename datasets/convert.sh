4d09d83fe4317d43dedb8f58a40670ad4cbf8192
Dmtrs Flaco Meng <tydeas.dr@gmail.com>
2020-03-10 01:52:17 +0200

    feat(3200): 3200 overlapping samples split by cj

--- datasets/convert.sh
+++ datasets/convert.sh
@@ -17,10 +17,16 @@ ffmpeg -i $url -ac 1 -ar $4 $converted
 
 mkdir $dataset_path
 length=$(ffprobe -i $converted -show_entries format=duration -v quiet -of csv="p=0")
-end=$(echo "$length / $chunk_size - 1" | bc)
-echo "splitting..."
-for i in $(seq 0 $end); do
-    ffmpeg -hide_banner -loglevel error -ss $(($i * $chunk_size)) -t $chunk_size -i $converted "$dataset_path/$i.wav"
+echo "splitting 3200 overlapping..."
+num=3200
+for i in $(seq 0 $num); do
+    time=$(echo "($i*($length - $chunk_size))/$num" | bc)
+    ffmpeg -hide_banner -loglevel error -ss $time -t $chunk_size -i $converted "$dataset_path/$i.wav"
 done
+# echo "splitting $end overlapping..."
+# end=$(echo "$length / $chunk_size - 1" | bc)
+#for i in $(seq 0 $end); do
+#    ffmpeg -hide_banner -loglevel error -ss $(($i * $chunk_size)) -t $chunk_size -i $converted "$dataset_path/$i.wav"
+# done
 echo "done"
 rm -f $converted
